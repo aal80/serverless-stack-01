8c4e967d5d9951431a05584d22337b26a6338fee
aal80 <aal80@users.noreply.github.com>
2024-09-30 10:56:48 -0500

    Update main.tf

--- lambda/main.tf
+++ lambda/main.tf
@@ -1,12 +1,29 @@
-data "archive_file" "this" {
-  type = "zip"
-  source_dir  = "${path.module}/../src"
-  output_path = "${path.module}/../../../tmp/function.zip"
-}
-
 resource "random_pet" "function_name" {
   prefix = "lambda-"
   length = 3
+}
+
+data "archive_file" "this" {
+  type = "zip"
+  source_dir  = "${path.module}/../src"
+  output_path = "${path.module}/../../../tmp/${random_pet.function_name}.zip"
+}
+
+
+data "local_file" "this" {
+  filename = data.archive_file.this.output_path
+}
+
+resource "aws_s3_object" "this" {
+  bucket = var.bucket_id
+
+  key    = "${random_pet.function_name}.zip"
+
+  content_base64 = data.local_file.this.content_base64
+  # source = data.archive_file.lambda_hello_world.output_path
+
+  etag = data.local_file.this.content_md5
+  # etag = filemd5(data.archive_file.lambda_hello_world.output_path)
 }
 
 resource "aws_iam_role" "this" {
@@ -38,8 +55,10 @@ resource "aws_iam_role" "this" {
    runtime = "nodejs18.x"
    handler = "index.handler"
 
-   filename = data.archive_file.this.output_path
-   source_code_hash = data.archive_file.this.output_base64sha256
+   s3_bucket = var.bucket_id
+   s3_key    = aws_s3_object.this.key
+
+    source_code_hash = data.archive_file.this.output_base64sha256
  }
 
  resource "aws_cloudwatch_log_group" "this" {
